9bdd2a083cc95cd709fc47b521c77b709d66997c
piotrmigas <piotr.migas@icloud.com>
2023-07-01 01:13:52 +0200

    commit

--- src/components/Buttons/DownvoteBtn.tsx
+++ src/components/Buttons/DownvoteBtn.tsx
@@ -4,31 +4,46 @@ import { useNavigate } from 'react-router-dom';
 import { useGetVotesQuery } from '../../redux/api';
 
 type Props = {
-  postId: string;
+  commentId?: string;
+  postId?: string;
   user: User;
 };
 
-const DownvoteBtn = ({ postId, user }: Props) => {
+const DownvoteBtn = ({ postId, user, commentId }: Props) => {
   const { data: votes } = useGetVotesQuery();
 
-  const isVotedByUser = votes?.find((vote) => vote.postId === postId && vote.uid === user?.uid);
+  const isVotedByUser = votes?.find((vote) =>
+    postId ? vote.postId === postId : vote.commentId === commentId && vote.uid === user?.uid
+  );
 
-  const typeOfVote = votes?.find((vote) => vote.postId === postId)?.type;
+  const typeOfVote = votes?.find((vote) => (postId ? vote.postId === postId : vote.commentId === commentId))?.type;
 
   const navigate = useNavigate();
 
   const vote = () => {
     if (!user) navigate('/login');
-    addDoc(collection(db, 'votes'), { postId, uid: user?.uid, type: 'down' });
-    updateDoc(doc(db, 'posts', postId), { voteScore: increment(-1) });
+    if (postId) {
+      addDoc(collection(db, 'votes'), { postId, uid: user?.uid, type: 'down' });
+      updateDoc(doc(db, 'posts', postId), { voteScore: increment(-1) });
+    } else {
+      addDoc(collection(db, 'votes'), { commentId, uid: user?.uid, type: 'down' });
+      updateDoc(doc(db, 'comments', commentId), { voteScore: increment(-1) });
+    }
   };
 
   const unvote = async () => {
     if (!user) navigate('/login');
-    const q = query(collection(db, 'votes'), where('postId', '==', postId), where('uid', '==', user?.uid));
-    const querySnapshot = await getDocs(q);
-    querySnapshot.forEach((doc) => deleteDoc(doc.ref));
-    updateDoc(doc(db, 'posts', postId), { voteScore: increment(1) });
+    if (postId) {
+      const q = query(collection(db, 'votes'), where('postId', '==', postId), where('uid', '==', user?.uid));
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => deleteDoc(doc.ref));
+      updateDoc(doc(db, 'posts', postId), { voteScore: increment(1) });
+    } else {
+      const q = query(collection(db, 'votes'), where('commentId', '==', commentId), where('uid', '==', user?.uid));
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => deleteDoc(doc.ref));
+      updateDoc(doc(db, 'comments', commentId), { voteScore: increment(1) });
+    }
   };
 
   return (
--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,8 +1,8 @@
 import { Link } from 'react-router-dom';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
-import UpvoteCommentBtn from './Buttons/UpvoteCommentBtn';
-import DownvoteCommentBtn from './Buttons/DownvoteCommentBtn';
+import UpvoteBtn from './Buttons/UpvoteBtn';
+import DownvoteBtn from './Buttons/DownvoteBtn';
 import { Comment } from '../redux/api';
 
 dayjs.extend(relativeTime);
@@ -17,9 +17,9 @@ const Comments = ({ postComments, user }: Props) => (
     {postComments.map(({ id, username, voteScore, createdAt, body }) => (
       <div className='flex' key={id}>
         <div className='flex-shrink-0 w-10 py-2 text-center rounded-l'>
-          <UpvoteCommentBtn commentId={id} user={user} />
+          <UpvoteBtn commentId={id} user={user} />
           <p className='text-xs font-bold'>{voteScore}</p>
-          <DownvoteCommentBtn commentId={id} user={user} />
+          <DownvoteBtn commentId={id} user={user} />
         </div>
         <div className='py-2 pr-2'>
           <p className='mb-1 text-xs leading-none'>
